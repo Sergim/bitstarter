e1eefe48a5acbe55bfb80034c12f749efc5719a7
Sergio Nascimento <sergim@gmail.com>
2013-07-10 01:43:34 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
   buffer = fs.readFileSync(index.html, utf8);
   response.send(buffer.toString());
